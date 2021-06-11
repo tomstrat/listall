99ea891238139c33e787ed2a21b52118caa345bb
Tom Strat Laptop <tomstratfic@hotmail.com>
2021-06-11 12:57:37 +0100

    added ignore functionality with config file

--- listall.js
+++ listall.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const chalk = require("chalk");
+const ignore = require("./config.json").ignore;
 
 const listAll = (path) => {
 
@@ -17,8 +18,11 @@ const listAll = (path) => {
 
 	const readFolder = (path) => {
 
-		//If folder is too large (and not the first) abrv it
-		if(fs.readdirSync(path).length > 20 && !first){
+		//If folder is ignored or too large (and not the first) abrv it
+		if(isIgnored(path.match(/\w+$/))){
+			console.log(chalk.grey.bold(`${(tabs + data) + path.match(/\w+$/)}...`));
+			return;
+		} else if(fs.readdirSync(path).length > 20 && !first){
 			console.log(chalk.red.bold(`${(tabs + data) + path.match(/\w+$/)}...`));
 			return;
 		} else if(!first){
@@ -44,6 +48,18 @@ const listAll = (path) => {
 
 	//initial call
 	readFolder(inputPath)
+}
+
+const isIgnored = (folder) => {
+	if(!folder){
+		return false;
+	}
+	for(let name of ignore) {
+		if (folder[0] === name){
+			return true;
+		}
+	}
+	return false;
 }
 
 var args = process.argv.slice(2).join("");
